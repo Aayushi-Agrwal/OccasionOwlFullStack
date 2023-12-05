7837381421deb66860c50e7d4b9a6ebe48983f94
Aayushi-Agrwal <khushiagarwal01464@gmail.com>
2023-12-05 19:54:01 +0530

    feat: login page

--- client/app/layouts/Login.tsx
+++ client/app/layouts/Login.tsx
@@ -7,41 +7,42 @@ interface props {
 }
 export const Login: React.FC<props> = ({ setLoginState }) => {
   return (
-    <main className="flex flex-col w-3/5 items-center justify-center h-screen bg-gradient-to-r from-sky-700 to-indigo-800">
-      <div className="fixed top-4 left-12 text-2xl bg-gradient-to-r from-blue-400 via-sky-400 to-indigo-200 inline-block text-transparent bg-clip-text">
-        OccasionOwl
-      </div>
-      <div className="w-1/2">
-        <h1 className="text-3xl font-medium tracking-wide mb-4">
-          <div className="animate-typing overflow-hidden whitespace-nowrap border-r-4 border-r-white text-white">
-            Welcome to OccasionOwl
-            <span className="text-sky-400 text-6xl">.</span>
-          </div>
-        </h1>
-        <p className="bg-gradient-to-r from-blue-200 via-sky-400 to-indigo-100 inline-block text-transparent bg-clip-text text-lg">
-          The ultimate event planning chat app. Connects users effortlessly,
-          enabling seamless collaboration. Simplifies planning for all
-          occasions. Enhance your events with OccasionOwl's intuitive interface
-          and real-time coordination features. Your perfect party is just a chat
-          away.
-        </p>
-      </div>
-      {/* <div className="flex justify-around w-screen">
-        <div className="text-left pb-4">
-          <h1 className="text-3xl font-medium tracking-wide">
-            Login to continue
-            <span className="text-sky-400 text-6xl">.</span>
+    <main className="flex">
+      <div className="flex flex-col w-3/5 items-center justify-center h-screen bg-gradient-to-r from-sky-700 to-indigo-800">
+        <div className="fixed top-4 left-12 text-2xl bg-gradient-to-r from-blue-400 via-sky-400 to-indigo-200 inline-block text-transparent bg-clip-text">
+          OccasionOwl
+        </div>
+        <div className="w-1/2">
+          <h1 className="text-3xl font-medium tracking-wide mb-4">
+            <div className="animate-typing overflow-hidden whitespace-nowrap border-r-4 border-r-white text-white">
+              Welcome to OccasionOwl
+              <span className="text-sky-400 text-6xl">.</span>
+            </div>
           </h1>
+          <p className="bg-gradient-to-r from-blue-200 via-sky-400 to-indigo-100 inline-block text-transparent bg-clip-text text-lg">
+            The ultimate event planning chat app. Connects users effortlessly,
+            enabling seamless collaboration. Simplifies planning for all
+            occasions. Enhance your events with OccasionOwl's intuitive
+            interface and real-time coordination features. Your perfect party is
+            just a chat away.
+          </p>
         </div>
+      </div>
+      <div className="flex flex-col justify-center items-center w-2/5">
+        <h1 className="text-3xl font-medium tracking-wide pb-6">
+          Get started
+          <span className="text-sky-400 text-6xl">.</span>
+        </h1>
 
-        <div
-          onClick={() => setLoginState(true)}
-          className="flex gap-2 text-2xl bg-violet-700 pb-2 pt-2 pr-4 pl-4 rounded-lg items-center transition hover:translate-y-1 cursor-pointer"
-        >
-          <FontAwesomeIcon icon={faGoogle} />
-          Sign in with Google
+        <div className="flex gap-8">
+          <div className="flex text-lg bg-violet-700 px-16 py-2 justify-center rounded-lg items-center transition hover:translate-y-0.5 cursor-pointer">
+            Log in
+          </div>
+          <div className="flex text-lg bg-violet-700 px-16 py-2 justify-center rounded-lg items-center transition hover:translate-y-0.5 cursor-pointer">
+            Sign up
+          </div>
         </div>
-      </div> */}
+      </div>
     </main>
   );
 };
