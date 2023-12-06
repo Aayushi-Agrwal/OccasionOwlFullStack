28fa08f99b930b19a6510167d123c82b88f0af8a
Aayushi-Agrwal <khushiagarwal01464@gmail.com>
2023-12-06 20:33:01 +0530

    feat: add login component

--- client/app/(routes)/login/page.tsx
+++ client/app/(routes)/login/page.tsx
@@ -1,5 +1,14 @@
-import LoginForm from "@/app/features/login/components/loginForm";
+import { LoginForm } from "@/app/features/login/components/loginForm";
 
 export default function Login() {
-  return <LoginForm />;
+  return (
+    <main className="w-screen h-screen">
+      <LoginForm
+        title="Welcome back"
+        buttonTitle="Log in"
+        message="Don't have an account?"
+        messageAction=" Sign up"
+      />
+    </main>
+  );
 }
