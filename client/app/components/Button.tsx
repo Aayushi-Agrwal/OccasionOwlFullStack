91bf11b51c762157ff047f035aa4932c57094a9f
Aayushi-Agrwal <khushiagarwal01464@gmail.com>
2023-12-06 22:06:35 +0530

    feat: add onclick prop

--- client/app/components/Button.tsx
+++ client/app/components/Button.tsx
@@ -1,9 +1,11 @@
 import { Url } from "next/dist/shared/lib/router/router";
 import Link from "next/link";
+import { MouseEventHandler } from "react";
 
 interface PropsBox {
   name: string;
   type: "button" | "submit" | "reset" | undefined;
+  onClick?: MouseEventHandler<HTMLButtonElement>;
 }
 
 interface PropsLink {
@@ -13,6 +15,7 @@ interface PropsLink {
 
 interface PropsBoxChildren {
   children: React.ReactNode;
+  onClick?: MouseEventHandler<HTMLButtonElement>;
 }
 
 export const LinkButton: React.FC<PropsLink> = ({ name, href }) => {
@@ -26,20 +29,27 @@ export const LinkButton: React.FC<PropsLink> = ({ name, href }) => {
   );
 };
 
-export const BoxButton: React.FC<PropsBox> = ({ name, type }) => {
+export const BoxButton: React.FC<PropsBox> = ({ name, type, onClick }) => {
   return (
     <button
       className="h-12 w-full flex text-lg bg-violet-700 px-16 py-2 justify-center rounded-lg items-center transition hover:translate-y-0.5 cursor-pointer"
       type={type}
+      onClick={onClick}
     >
       {name}
     </button>
   );
 };
 
-export const BoxButtonWhite: React.FC<PropsBoxChildren> = ({ children }) => {
+export const BoxButtonWhite: React.FC<PropsBoxChildren> = ({
+  children,
+  onClick,
+}) => {
   return (
-    <button className="h-12 w-full flex gap-4 text-lg border-2 border-white py-2 justify-center rounded-lg items-center transition hover:translate-y-0.5 cursor-pointer">
+    <button
+      className="h-12 w-full flex gap-4 text-lg border-2 border-white py-2 justify-center rounded-lg items-center transition hover:translate-y-0.5 cursor-pointer"
+      onClick={onClick}
+    >
       {children}
     </button>
   );
