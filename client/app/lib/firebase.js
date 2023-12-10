42e5990c3985384eac7ce47e8b3284930fd3aab0
Aayushi-Agrwal <khushiagarwal01464@gmail.com>
2023-12-10 20:49:29 +0530

    fix: add env file

--- client/app/lib/firebase.js
+++ client/app/lib/firebase.js
@@ -8,18 +8,18 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAn4WTG9oJKLrhpySOvLU2iZ-aDe_63ULE",
-  authDomain: "ocassionowl.firebaseapp.com",
-  projectId: "ocassionowl",
-  storageBucket: "ocassionowl.appspot.com",
-  messagingSenderId: "981295651403",
-  appId: "1:981295651403:web:272afa40716bea3d04cbce",
-  measurementId: "G-B75JC2KKT6",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
+const analytics = getAnalytics(app);
 
 export const auth = getAuth(app);
 export default app;
--- client/app/page.tsx
+++ client/app/page.tsx
@@ -3,15 +3,11 @@
 import Chatroom from "@/app/features/chatroom/page";
 import { Auth } from "@/app/features/auth/components/authScreen";
 import useFirebaseUser from "./hooks/useFirebaseUser";
-// import { FirebaseAuthProvider } from "./hooks/authContextProvider";
+
+const dotenv = require("dotenv");
+dotenv.config();
 
 export default function Home() {
   const user = useFirebaseUser();
-  // const pathname = usePathname();
-  // const isApp = pathname.startsWith("/app");
-  return (
-    // <FirebaseAuthProvider>
-    <>{user != null ? <Chatroom /> : <Auth />}</>
-    // </FirebaseAuthProvider>
-  );
+  return <>{user != null ? <Chatroom /> : <Auth />}</>;
 }
