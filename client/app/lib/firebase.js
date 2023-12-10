ba0cf7cf76818a0c8d7cc09a8717da2b15e34d4a
Aayushi-Agrwal <khushiagarwal01464@gmail.com>
2023-12-10 13:09:26 +0530

    fix: update firebase config

--- client/app/lib/firebase.js
+++ client/app/lib/firebase.js
@@ -1,25 +1,52 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAuth } from "firebase/auth";
+// import { getAnalytics } from "firebase/analytics";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAn4WTG9oJKLrhpySOvLU2iZ-aDe_63ULE",
+//   authDomain: "ocassionowl.firebaseapp.com",
+//   projectId: "ocassionowl",
+//   storageBucket: "ocassionowl.appspot.com",
+//   messagingSenderId: "981295651403",
+//   appId: "1:981295651403:web:272afa40716bea3d04cbce",
+//   measurementId: "G-B75JC2KKT6",
+// };
+
+// if (!firebase.apps.length) {
+//   firebase.initializeApp(firebaseConfig);
+//   if(typeof window )
+// }
+
+// // Initialize Firebase
+// // const app = initializeApp(firebaseConfig);
+// // const analytics = getAnalytics(app);
+
+// // export const auth = getAuth(app);
+// // export default app;
+
+import { getApps, initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
 const firebaseConfig = {
-  apiKey: "AIzaSyAn4WTG9oJKLrhpySOvLU2iZ-aDe_63ULE",
-  authDomain: "ocassionowl.firebaseapp.com",
-  projectId: "ocassionowl",
-  storageBucket: "ocassionowl.appspot.com",
-  messagingSenderId: "981295651403",
-  appId: "1:981295651403:web:272afa40716bea3d04cbce",
-  measurementId: "G-B75JC2KKT6",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-
-export const auth = getAuth(app);
-export default app;
+if (!getApps.length) {
+  initializeApp(firebaseConfig);
+  if (typeof window !== "undefined") {
+    if ("measurementId" in firebaseConfig) {
+      getAnalytics();
+    }
+  }
+}
--- client/app/page.tsx
+++ client/app/page.tsx
@@ -4,6 +4,9 @@ import Chatroom from "@/app/features/chatroom/page";
 import { Auth } from "@/app/features/auth/components/authScreen";
 import useFirebaseUser from "./hooks/useFirebaseUser";
 import { usePathname } from "next/navigation";
+import "@/app/lib/firebase";
+const dotenv = require("dotenv");
+dotenv.config();
 
 export default function Home() {
   const user = useFirebaseUser();
